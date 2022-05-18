3fe17be41b2711ec1022a1771abb7f2519a52a02
marangY <jwkid9124@gmail.com>
2022-05-18 10:23:14 +0900

    add HaNaBBaGi

--- Java/H1/Main.java
+++ Java/H1/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void whatGame(RockPaperScissors rsp) {
         if (rsp instanceof MuckJjiPa) {
             System.out.println("\n[묵찌빠 실행]");
+        } else if (rsp instanceof HaNaBbaGi) {
+            System.out.println("\n[하나빼기 실행]");
         } else {
             System.out.println("\n[가위바위보 실행]");
         }
@@ -15,7 +17,7 @@ public class Main {
         RockPaperScissors actGame;
 
         while (true) {
-            System.out.print("[할 게임을 선택 하세요]\n\n[1. 가위 바위 보] [2. 묵찌빠] [종료 : q]\n\n입력 : ");
+            System.out.println("[할 게임을 선택 하세요]\n\n[1. 가위 바위 보] [2. 묵찌빠] [3. 하나빼기] [종료 : q]");
             String select = scanner.next();
 
             if (select.equals("q")) {
@@ -25,6 +27,8 @@ public class Main {
                 actGame = new RockPaperScissors();
             } else if (select.equals("2")) {
                 actGame = new MuckJjiPa();
+            } else if (select.equals("3")){
+                actGame = new HaNaBbaGi();
             } else {
                 System.out.println("\n[알 수 없는 명령어 입니다.]\n");
                 continue;
