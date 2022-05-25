d5d155fdbe8408806b8443fb3a90bd748926f383
marangY <jwkid9124@gmail.com>
2022-05-25 14:52:21 +0900

    Delete test.java

--- Java/sj3/test.java
+++
@@ -1,8 +0,0 @@
-import java.util.LinkedList;
-
-public class test {
-    public static void main(String[] args) {
-        LinkedList<Item> itemArray = new LinkedList<Item>();
-        itemArray.add(new Item("a", 1, 2));
-    }
-}
